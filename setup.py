4b8dd63327fd2e7b2939b2de492685d11f070972
bajoukx <jose.a.d@gmail.com>
2023-07-02 00:44:49 +0200

    FIX. setuptools

--- setup.py
+++ setup.py
@@ -10,4 +10,5 @@ setup(name='proton3',
       version='0.1.0',
       description='A package for the proton3 project.',
       author='bajoukx',
-      install_requires=REQUIRED_PACKAGES)
+      install_requires=REQUIRED_PACKAGES,
+      packages=['proton3'])
