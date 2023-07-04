b06afc95fea9fb85ce930c22e1870cb79a34622e
bajoukx <jose.a.d@gmail.com>
2023-07-04 23:20:13 +0200

    VER. update version reflecting streamlit hosting.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ REQUIRED_PACKAGES = [
 ]
 
 setup(name='proton3',
-      version='0.1.0',
+      version='0.1.1',
       description='A package for the proton3 project.',
       author='bajoukx',
       install_requires=REQUIRED_PACKAGES,
