c6e19f70cb8e482b5261f9ea297c86eaaf3743d0
Bajoukx <67920071+Bajoukx@users.noreply.github.com>
2023-07-24 15:36:51 +0200

    ADD. renamed constants and added doc

---
+++ proton3/constants/atomic_units.py
@@ -0,0 +1,18 @@
+"""Atomic units constants used in proton3.
+
+Atomic Units are a system of units where an electron rest mass, the
+elementary charge, the reduced Planck's constant and the Coulomb force constant
+are all equal to 1.
+
+For more information see:
+https://chem.libretexts.org/Courses/Pacific_Union_College/Quantum_Chemistry/08%3A_Multielectron_Atoms/8.01%3A_Atomic_and_Molecular_Calculations_are_Expressed_in_Atomic_Units
+"""
+
+ELECTRON_MASS = 1.0
+ELEMENTARY_CHARGE = 1.0
+REDUCED_PLANCK = 1.0
+COULOMB_FORCE = 1.0
+
+ANGSTROM =  1.8897261246257702
+ELECTRONVOLT = 0.03674932217565499
+FEMTOSECOND = 4.134137333518212 * 10.
